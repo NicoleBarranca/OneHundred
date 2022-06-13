33ea19bfb9da9386590434554a522345f0f10f8e
Nicole Barranca <nikkibarranca@gmail.com>
2022-06-13 14:43:28 -0400

    test get all route

--- routes/sushi.js
+++ routes/sushi.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 // Getting all
+router.get("/", (req, res) => {
+  res.send("Hello Wurld");
+});
+
 // Getting One
+router.get("/:id", (req, res) => {});
+
 // Creating One
-// Updating One
+router.post("/", (req, res) => {});
+
+// Updating One (can also user .patch)
+router.put("/", (req, res) => {});
+
 // Deleting one
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
