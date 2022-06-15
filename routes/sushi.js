ecbfa8eceb0f6db64b3485b4f380a86da506983b
Nicole Barranca <nikkibarranca@gmail.com>
2022-06-15 13:58:06 -0400

    put request

--- routes/sushi.js
+++ routes/sushi.js
@@ -1,21 +1,59 @@
 const express = require("express");
 const router = express.Router();
+const Roll = require("../models/roll");
 
 // Getting all
-router.get("/", (req, res) => {
-  res.send("Hello Wurld");
+router.get("/", async (req, res) => {
+  try {
+    const sushi = await Roll.find();
+    res.json(sushi);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Getting One
-router.get("/:id", (req, res) => {});
+router.get("/:id", getRoll, (req, res) => {
+  res.send(res.roll.name);
+});
 
 // Creating One
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const roll = new Roll({
+    name: req.body.name,
+    ingredients: req.body.ingredients,
+    price: req.body.price,
+  });
+
+  try {
+    const newRoll = await roll.save();
+    res.status(201).json(newRoll);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // Updating One (can also user .patch)
 router.put("/", (req, res) => {});
 
 // Deleting one
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  res.roll;
+});
+
+// middleware
+async function getRoll(req, res, next) {
+  let roll;
+  try {
+    roll = await Roll.findById(req.params.id);
+    if (roll == null) {
+      return res.status(404).json({ message: "Cannot find Roll" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.roll = roll;
+  next();
+}
 
 module.exports = router;
